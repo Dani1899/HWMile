c799ece3f267922c670a96cc6931a872da7bb3ba
Neto test <danilkondakov30@mail.ru>
2023-01-30 16:29:41 +0300

    git commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         int amount1 = 13680;//Стоимость билета
         int amount2 = 20;//Количество рублей для одной бонусной мили
         int bonus = amount1 / amount2;//количество бонусных миль
-        System.out.println("Итоговый бонус:" + bonus);
+        System.out.println("Итоговый бонус:" + bonus + " рубля");
     }
 }
