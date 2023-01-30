b8c9077c363ed7b9546dddd73b639e4defacd6a0
Dani1899 <120176482+Dani1899@users.noreply.github.com>
2023-01-30 16:31:17 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int amount1 = 13680;//Стоимость билета
         int amount2 = 20;//Количество рублей для одной бонусной мили
-        int bonus = amount1 / amount2;//количество бонусных миль
+        int bonus = amount1 / amount2;//Количество бонусных миль
         System.out.println("Итоговый бонус:" + bonus + " рубля");
     }
 }
